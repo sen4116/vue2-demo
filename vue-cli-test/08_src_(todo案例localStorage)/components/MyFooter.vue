156b904d5b025ea9c9940f34152f8fe98611014b
sen4116 <1477715879@qq.com>
2022-08-08 11:25:54 +0800

    localstorage

---
+++ vue-cli-test/08_src_(todo案例localStorage)/components/MyFooter.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="footerBox" v-show="total">
+    <div>
+      <label for="">
+        <!-- 使用methods方法 -->
+        <!-- <input type="checkbox" class="checkBox" :checked="isAll"  @change="checkAll"/> -->
+        <!-- 使用计算属性场景：当遇到一个输入类型的节点，输入的初始值需要展示，并且有用户交互事件，
+            可以使用v-model和computed 搭配使用 -->
+        <input type="checkbox" class="checkBox" v-model="isAll"/>
+      </label>
+      <span class="footerText">
+        <span>已完成{{ count }}</span
+        >/全部{{ total}}
+      </span>
+    </div>
+    <button class="delete" @click="clearTodo">清除已完成任务</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyFooter",
+  props: ["todoData","checkAllTodo","clearAllTodo"],
+  computed: {
+    total(){
+      return this.todoData.length
+    },   
+    count() {
+      return this.todoData.reduce((pre, current) => {
+        return pre + (current.done ? 1 : 0);
+      }, 0);
+    },
+    isAll:{
+      get(){
+        return this.total === this.count && this.total > 0
+      },
+      set(value){
+        this.checkAllTodo(value)
+      }
+    }
+  },
+  methods:{
+    checkAll(e){
+      this.checkAllTodo(e.target.checked)
+    },
+    clearTodo(){
+      this.clearAllTodo()
+    }
+  }
+};
+</script>
+
+<style scoped>
+.footerBox {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.footerText {
+  margin-left: 20px;
+}
+</style>
---
+++ vue-cli-test/08_src_(todo案例localStorage)/components/MyItem.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div class="listItem">
+      <li>
+        <label>
+          <input type="checkbox" :checked="data.done" class="checkBox" @change="handleCheck(data.id)"/>
+          <span>{{ data.title }}</span>
+        </label>
+      </li>
+      <button class="delete" @click="handleDetest(data.id)">删除</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyItem",
+  data() {
+    return {
+      data: this.todo,
+    };
+  },
+  props: {
+    todo: Object,
+    checkTodo:Function,
+    detestTodo:Function,
+  },
+  methods: {
+    //勾选or取消 checkbox事件
+    handleCheck(id){
+      this.checkTodo(id)
+    },
+    // 删除一个todo 
+    handleDetest(id){
+      // 确认为true，取消为false  confirm：浏览器对话框
+      if(!confirm('是否删除本事件?')) return 
+      this.detestTodo(id)
+    }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+
+
+.listItem {
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  line-height: 28px;
+  padding: 5px;
+  border: 1px solid #ddd;
+  box-sizing: border-box;
+  position: relative;
+  button{
+    display: none;
+  }
+}
+li {
+  list-style: none;
+}
+
+.listItem:hover{
+  background-color: #ddd;
+  button{
+    display: block;
+  }
+}
+
+</style>
