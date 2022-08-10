b92af836716409468c2c5577270724c1955e9a8e
sen4116 <1477715879@qq.com>
2022-08-10 17:32:47 +0800

    todo案例消息订阅版

---
+++ vue-cli-test/14_src_(todo案例消息订阅版本)/components/MyList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="list">
+    <ul>
+       <MyItem 
+       v-for="todoObj in todoData" 
+       :key="todoObj.id" 
+       :todo="todoObj"
+       ></MyItem> 
+    </ul>
+  </div>
+</template>
+
+<script>
+    import MyItem from './MyItem.vue'
+    export default {
+        name:'MyList',
+        data(){
+            return {
+                
+            }
+        },
+        components:{
+            MyItem
+        },
+        props:['todoData']
+    }
+</script>
+
+<style lang="less" scoped>
+.list{
+    padding: 0;
+}
+ul{
+    padding-left:0;
+
+}
+</style>
