68f3c9aa9227fdc09fad35cbd0c2265f8bf3c113
sen4116 <147715879@qq.com>
2022-08-15 09:33:45 +0800

    代理服务

---
+++ vue-cli-test/16_src_代理服务/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button @click="getFiction">获取小说人物</button>
+    <button @click="getCars">获取汽车品牌</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  methods: {
+    getFiction(){
+      axios.get('http://localhost:8080/fiction/server').then(
+        response =>{
+          console.log(response.data)
+        },
+        error => {
+          console.log(error)
+        }
+      )
+    },
+    getCars(){
+      axios.get('http://localhost:8080/car/server').then(
+        response =>{
+          console.log(response.data)
+        },
+        error => {
+          console.log(error)
+        }
+      )
+    }
+  },
+}
+</script> 
+
+<style>
+
+</style>
--- vue-cli-test/src/App.vue
+++ vue-cli-test/src/App.vue
@@ -1,37 +1,12 @@
 <template>
-  <div>
-    <button @click="getFiction">获取小说人物</button>
-    <button @click="getCars">获取汽车品牌</button>
-  </div>
+  
 </template>
 
 <script>
-import axios from 'axios'
 export default {
-  methods: {
-    getFiction(){
-      axios.get('http://localhost:8080/fiction/server').then(
-        response =>{
-          console.log(response.data)
-        },
-        error => {
-          console.log(error)
-        }
-      )
-    },
-    getCars(){
-      axios.get('http://localhost:8080/car/server').then(
-        response =>{
-          console.log(response.data)
-        },
-        error => {
-          console.log(error)
-        }
-      )
-    }
-  },
+
 }
-</script> 
+</script>
 
 <style>
 
