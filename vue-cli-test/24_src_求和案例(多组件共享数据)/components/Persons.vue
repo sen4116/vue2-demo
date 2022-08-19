44a002e9fe394905384015e98b33a6577b93c587
sen4116 <147715879@qq.com>
2022-08-19 11:01:49 +0800

    多组件共享数据

---
+++ vue-cli-test/24_src_求和案例(多组件共享数据)/components/Persons.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>人员列表</h1>
+    <h2 style="color:red">count组件的和是: {{sum}}</h2>
+    <input type="text" placeholder="请输入名字" v-model="name"> 
+    <button @click="add">添加</button>
+    <ul>
+        <li v-for="p in personList" :key="p.id">
+            {{p.name}}
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {nanoid} from 'nanoid'
+export default {
+    data() {
+        return {
+            name:''
+        }
+    },
+    computed:{
+        personList(){
+            return this.$store.state.personList
+        },
+        sum(){
+            return this.$store.state.sum
+        }
+    },
+    methods: {
+        add(){
+            const personObj = {id:nanoid(),name:this.name}
+            this.$store.commit('ADD_PERSON',personObj)
+            this.name =""
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
